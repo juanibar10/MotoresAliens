adcab277e96c4546dc68212a1fe83cc31f4a2239
juanibar10 <47811666+juanibar10@users.noreply.github.com>
2020-04-22 19:19:40 +0200

    proyecto

---
+++ Motores Shooter/Assets/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Material mat;
+    public Transform player;
+    public float revealSpeed;
+    public AnimationCurve curve;
+    float contador = 0;
+    private void Awake()
+    {
+        mat.SetInt("end", 1);
+        mat.SetVector("PlayerPos", player.position);
+
+    }
+    private void Update()
+    {
+        if(contador < 500)
+        {
+            contador += Time.deltaTime * revealSpeed;
+        }
+        else
+        {
+            contador = 500;
+            mat.SetInt("end", 0);
+        }
+
+        mat.SetFloat("revealScale", Mathf.Lerp(0, 1000, curve.Evaluate(contador / 500))); 
+
+    }
+}
---
+++ Motores Shooter/Assets/Scripts/ZombieAI.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieAI : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator anim;
+
+    public Transform target;
+
+    public bool hitting;
+    public bool jumping;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        anim.SetFloat("Vel", 1);
+        agent.SetDestination(target.position);
+    }
+
+    public Transform SetTarget
+    {
+        set{ target = value; }
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(target.position);
+
+        if (agent.isOnOffMeshLink && !jumping)
+            StartCoroutine(JumpBarrier(0.7f));
+
+        if (agent.remainingDistance <= agent.stoppingDistance && target.tag == "Player" && !hitting)
+            StartCoroutine(Hit());
+
+    }
+
+    IEnumerator JumpBarrier(float tiempo)
+    {
+        print("Empieza");
+        Vector3 startPos = transform.position;
+        Vector3 endPos = agent.currentOffMeshLinkData.endPos;
+
+        jumping = true;
+        agent.isStopped = true;
+        anim.SetTrigger("Jump");
+
+        float forTime = 50f;
+
+        for (int i = 0; i <= forTime; i++)
+        {
+            transform.position = new Vector3(Mathf.Lerp(startPos.x, endPos.x, i * (1f / forTime)), transform.position.y, Mathf.Lerp(startPos.z, endPos.z, i * (1f / forTime)));
+            yield return new WaitForSeconds(tiempo * (1f / forTime));
+        }
+
+        agent.isStopped = false;
+        agent.CompleteOffMeshLink();
+
+        yield return new WaitUntil(() => !agent.isOnOffMeshLink);
+        jumping = false;
+    }
+
+    IEnumerator Hit()
+    {
+        hitting = true;
+        agent.isStopped = true;
+        anim.SetFloat("Vel", 0);
+        yield return new WaitForEndOfFrame();
+
+        print("Animacion golpe");
+
+        yield return new WaitForSeconds(2);
+
+        anim.SetFloat("Vel", 1);
+        agent.isStopped = false;
+        agent.SetDestination(target.position);
+        hitting = false;
+    }
+}
